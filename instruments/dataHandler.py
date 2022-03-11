5a440dbbe28685b81f6eda4d4f9580ee39417a5d
AllisonPessoa <66957124+AllisonPessoa@users.noreply.github.com>
2022-03-11 10:27:40 -0300

    improved usability
    Ged rid of most unecessary things for creating a new instrument. the user must be able to easily create and communicate with the other classes.

--- instruments/dataHandler.py
+++ instruments/dataHandler.py
@@ -10,25 +10,28 @@ class DataHandler(ABC):
     
     @abstractmethod
     def setDataParams(self, Xdim, Ydim):
+    """ Creates the data set for a scan, Xdim and Ydim represent the scan matrix dimensions. For example, hyperspectral scanning uses a (Xdim, Ydim, 1024) numpy array. Each pixel represents a spectrum"""
         pass
     
     @abstractmethod
-    def setScanIndexPath(self, scanIndexPath):
-        pass
-    
-    @abstractmethod
-    def getDataDuringScan(self):
+    def getDataDuringScan(self, indexPos):
+    """ Returns the data to be shown during the scan.  It is possible to return an image data and a plot data in this order. Plot data can be omitted. 
+    Image data structure is a multidimensional (Xdim, Ydim) numpy array with floats eg. np.ones((Xdim,Ydim)). 
+    Plot data sctructure is a (X_data, Y_data) floats """
         pass
     
     @abstractmethod
     def getCurveData(self, indexPos):
+    """ If there is a plot data for each pixel (indexPos representing the index position), return the plot data numpy array with this method. If it is now the case, retur None """
         pass
     
     @abstractmethod
     def getRawData(self):
+    """ Returns the numpy data structure to be saved """
         pass
     
     @abstractmethod
     def close(self):
+    """ Close the communication with the instruments securetely """
         pass
     
