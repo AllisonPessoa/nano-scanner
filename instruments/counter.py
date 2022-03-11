47a2e0471256f50e5ecb669c0b2acf1337307bbb
AllisonPessoa <66957124+AllisonPessoa@users.noreply.github.com>
2022-03-11 10:36:52 -0300

    Removed index position from instruments

--- instruments/counter.py
+++ instruments/counter.py
@@ -44,15 +44,10 @@ class Counter(QtWidgets.QWidget, DataHandler, metaclass=FinalMeta):
         self.counterBuffer = deque([],maxlen=100)
         logger.info("Counter got data params")
         
-    def setScanIndexPath(self, scanIndexPath):
-        #scanIndexPath : List of tuples representing the scan current index position
-        self.scanIterator = iter(scanIndexPath)
-    
-    def getDataDuringScan(self):
+    def getDataDuringScan(self, indexPos):
         time.sleep(float(self.lineEdit_stepDelay.text())/1000) #seconds
         singleValue = self._acquireData()
-        curScanPos = next(self.scanIterator)
-        self._setPixelData(curScanPos, singleValue)
+        self._setPixelData(indexPos, singleValue)
         
         imageData = self._getIntensityMap()
         curveData = self._getDataBuffer()
